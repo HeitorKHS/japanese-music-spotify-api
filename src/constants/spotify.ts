a5c9f062c949f2b07d5cf179ee5d37de803eacd2
unknown <heitorkhs@gmail.com>
2025-12-15 19:42:38 -0300

    Create lib spotify

--- src/constants/spotify.ts
+++ src/constants/spotify.ts
@@ -1,41 +1,41 @@
 //Genres related to Japanese music
 export const JAPANESE_GENRES = [
-    'j-pop',
-    'j-rock',
-    'j-idol',
-    'j-dance',
-    'rap japonês',
-    'anime',
-    'japanese r&b',
-    'shibuya-kei',
-    'vocaloid',
-    'enka',
-    'city pop',
-    'anime',
+    "j-pop",
+    "j-rock",
+    "j-idol",
+    "j-dance",
+    "rap japonês",
+    "anime",
+    "japanese r&b",
+    "shibuya-kei",
+    "vocaloid",
+    "enka",
+    "city pop",
+    "anime",
 ] as const; //
 
 //Markets (countries)
-export const SPOTIFY_MARKET = 'PT';
+export const SPOTIFY_MARKET = "PT";
 
 //Playslist
 export const PLAYLISTS = {
-  TOKYO_SUPER_HITS: '0KgrxJF5QFmJViO5TJEfLr',
-  JAPANESE_WOMEN: '11WH19OqPnO6LAYKGJKYlJ',
-  ROCK_JAPAN: '59sn8iwLmvqjO83uT4w29S',
-  RAP_JAPAN: '61tMffmfWF4ejTy2UioY5D',
-  INDIE_JAPAN: '1zr44X4dTtCQ6thRYiQTej',
+  TOKYO_SUPER_HITS: "0KgrxJF5QFmJViO5TJEfLr",
+  JAPANESE_WOMEN: "11WH19OqPnO6LAYKGJKYlJ",
+  ROCK_JAPAN: "59sn8iwLmvqjO83uT4w29S",
+  RAP_JAPAN: "61tMffmfWF4ejTy2UioY5D",
+  INDIE_JAPAN: "1zr44X4dTtCQ6thRYiQTej",
  } as const;
 
 //Spotify API URLs
 export const SPOTIFY_API = {
-    BASE_URL: 'https://api.spotify.com/v1',
-    AUTH_URL: 'https://accounts.spotify.com/api/token',
+    BASE_URL: "https://api.spotify.com/v1",
+    AUTH_URL: "https://accounts.spotify.com/api/token",
 } as const;
 
 // Error messages
 export const ERROR_MESSAGES = {
-    AUTH_FAILED: 'Falha na autenticação com Spotify',
-    FETCH_FAILED: 'Erro ao buscar dados do Spotify',
-    NOT_FOUND: 'Conteúdo não encontrado',
-    RATE_LIMIT: 'Muitas requisições. Tente novamente em alguns segundos.',
+    AUTH_FAILED: "Falha na autenticação com Spotify",
+    FETCH_FAILED: "Erro ao buscar dados do Spotify",
+    NOT_FOUND: "Conteúdo não encontrado",
+    RATE_LIMIT: "Muitas requisições. Tente novamente em alguns segundos.",
 } as const;
---
+++ src/lib/spotify/client.ts
@@ -0,0 +1,103 @@
+import { SPOTIFY_API, ERROR_MESSAGES } from "@/src/constants/spotify";
+
+interface SpotifyAuthRespose{
+    access_token: string,
+    token_type: "Bearer",
+    expires_in: number,
+};
+
+//Verify token
+async function getAccessToken(): Promise<string>{
+
+    const clientId = process.env.SPOTIFY_CLIENT_ID;
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+    if(!clientId || !clientSecret){
+        throw new Error ("Credenciais doSpotify não configuradas");
+    }
+
+    //Makes the authentication request
+    try{
+
+        const response = await fetch(SPOTIFY_API.AUTH_URL, {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+            },
+            body: "grant_type=client_credentials",
+            next:{
+                revalidate: 3600,
+                tags: ["spotify_auth_token"],
+            },
+        });
+
+        if(!response.ok){
+            throw new Error(ERROR_MESSAGES.AUTH_FAILED);
+        }
+
+        const data: SpotifyAuthRespose = await response.json();
+
+        console.log(`Toekn obtido. Válido por ${data.expires_in}s`);
+
+        return data.access_token;
+
+    } catch (error){
+        console.error('Erro na autenticação: ', error);
+        throw new Error(ERROR_MESSAGES.AUTH_FAILED);
+    }
+
+}
+
+//Wrapper to make HTTP requests to the Spotify API
+export async function spotifyFetch<T>(endpoint: string, options?: RequestInit): Promise<T>{
+
+    const token = await getAccessToken();
+
+    const url = `${SPOTIFY_API.BASE_URL}${endpoint}`;
+
+    try{
+
+        const response = await fetch(url, {
+            ...options, //Spreads the other request options, such as method, credentials, etc
+            headers:{
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+                ...options?.headers,
+            },
+            next:{
+                revalidate: 3600,
+                tags: ["spotify_data"],//Tag to invalidate
+            },
+        });
+
+        if(!response.ok){
+            if(response.status === 429){
+                throw new Error(ERROR_MESSAGES.RATE_LIMIT);
+            }
+            if(response.status === 404){
+                throw new Error(ERROR_MESSAGES.NOT_FOUND);
+            }
+            throw new Error(`${ERROR_MESSAGES.FETCH_FAILED} (Status: ${response.status})`); 
+        }
+
+        return await response.json();
+
+    } catch (error){
+        console.error("Erro ao buscar dados: ", error);
+        throw error; //Rethrowing the same error that was caught
+    }
+
+}
+
+//Construct query parameters
+export function buildQueryString(params: Record<string, string | number | undefined>){
+
+    const filteredParams = Object.entries(params)//Converts the params object to an array of key-value pairs.
+    .filter(([_, value]) => value !== undefined )//Remove undefined values
+    .map(([key, value]) =>  `${key}=${encodeURIComponent(value!)}`)//.map is used to build a string of type key=value, but with the values ​​being encoded with encodeURIComponent
+    .join('&');//.join('&') takes all the elements of an array and joins them into a single string, separating them with the & character
+
+    return filteredParams ? `?${filteredParams}` :  "";
+
+}
