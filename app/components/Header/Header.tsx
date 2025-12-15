fdb9f27927c2353d286e5991173eb5b0bef1fdf6
unknown <heitorkhs@gmail.com>
2025-12-15 16:26:23 -0300

    Header creation

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export function Header(){
+
+    return(
+        
+        <header className="fixed top-0 left-0 right-0 z-50 bg-black h-16">
+            <div className="header-container py-3 flex items-center justify-start">
+                <div className="h-10">
+                    <Link href={"/"} aria-label="Ir para página inicial">
+                        <Image
+                            src={"/img/jmusic.png"}
+                            alt={"Logo"}
+                            width={849}
+                            height={285}
+                            className="h-full w-auto object-contain"
+                            priority
+                        />
+                    </Link>
+                </div>
+            </div>
+        </header>
+
+    )
+
+}
---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Header } from "./components/Header/Header";
+
+export const metadata: Metadata = {
+  title: "J-Music",
+  description: "Plataforma de música integrada com Spotify",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>
+          <Header/>
+          <main className="min-h-screen pt-16">
+            {children}
+          </main>  
+      </body>
+    </html>
+  );
+}
